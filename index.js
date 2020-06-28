3788bbad3dfea1c19226785dc181ef1dc2ab4aee
Savitha <savitha.gollamudi@gmail.com>
2020-06-28 22:17:14 +0530

    basic search api on input req

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com", {
+    params: {
+      apikey: "a79da565",
+      s: searchTerm,
+    },
+  });
+  console.log(response.data);
+};
+
+const input = document.querySelector("input");
+//this logic makes a request for every keypress -> this is bad
+input.addEventListener("input", (event) => {
+  //capture the input and send it as a search term
+  fetchData(event.target.value);
+});
