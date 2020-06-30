716389b44f285aca8c548742468f8a721c7d690d
Savitha <savitha.gollamudi@gmail.com>
2020-06-30 19:18:33 +0530

    close autocomplete widget

--- index.js
+++ index.js
@@ -29,18 +29,30 @@ const resultsWrapper = document.querySelector(".results");
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
+  //clears data after search, instead of appending to previous list
   resultsWrapper.innerHTML = "";
   dropdown.classList.add("is-active");
 
   for (let movie of movies) {
     const option = document.createElement("a");
+    //some images dont have posters.
     const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
     option.classList.add("dropdown-item");
     option.innerHTML = `
-    <img src = "${imgSrc}" />
-    ${movie.Title}
+    <img src = "${imgSrc}" />${movie.Title}
     `;
     resultsWrapper.appendChild(option);
   }
 };
 input.addEventListener("input", debounce(onInput, 500));
+//close the dropdown by clicking anywhere on screen
+//other than dropdown itself
+document.addEventListener("click", (event) => {
+  //event.target -> gives access to the current clicked element
+  //if you inspect and select an element .. you get $0 -> which iindicates selection of current element
+  //event.target === $0
+  //try document.contains($0) -> returns truw, coz we have that element selected.
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
