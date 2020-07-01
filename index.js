d3222d2a168ca9821338afdda6d596dc97a3a3fa
Savitha <savitha.gollamudi@gmail.com>
2020-07-02 00:46:03 +0530

    Show stats of a particular movie

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const fetchData = async (searchTerm) => {
   if (response.data.Error) {
     return [];
   }
-  // a promise is returned, since the function is async
   return response.data.Search;
 };
 
@@ -49,6 +48,7 @@ const onInput = async (event) => {
     option.addEventListener("click", () => {
       dropdown.classList.remove("is-active");
       input.value = movie.Title;
+      onMovieSelect(movie);
     });
     resultsWrapper.appendChild(option);
   }
@@ -61,3 +61,50 @@ document.addEventListener("click", (event) => {
     dropdown.classList.remove("is-active");
   }
 });
+//show statisticsof a movie when selected from dropdown
+const onMovieSelect = async (movie) => {
+  const response = await axios.get("http://www.omdbapi.com", {
+    params: {
+      apikey: "a79da565",
+      i: movie.imdbID,
+    },
+  });
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+  return `
+  <article class="media">
+  <figure class="media-left">
+    <p class="image"><img src="${movieDetail.Poster}"></p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <h1>${movieDetail.Title}</h1>
+      <h4>${movieDetail.Genre}</h4>
+      <p>${movieDetail.Plot}</p>
+    </div>
+  </div>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.Awards}</p>
+  <p class="subtitle">Awards</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.BoxOffice}</p>
+  <p class="subtitle">Box Office</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.Metascore}</p>
+  <p class="subtitle">Metascore</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.imdbRating}</p>
+  <p class="subtitle">IMDB Rating</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.imdbVotes}</p>
+  <p class="subtitle">IMDB Votes</p>
+</article>
+  `;
+};
