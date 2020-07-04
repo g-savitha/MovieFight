61d14f152a009ce0bd8406570b727b2a272a6ac0
Savitha <savitha.gollamudi@gmail.com>
2020-07-04 20:21:49 +0530

    Refactor autocomplete - root and rendering options

---
+++ autocomplete.js
@@ -0,0 +1,48 @@
+const createAutoComplete = ({ root, renderOption }) => {
+  root.innerHTML = `
+<label for="search"><b>Search for a movie</b></label>
+<input type="text" class="input" />
+<div class="dropdown">
+  <div class="dropdown-menu">
+    <div class="dropdown-content results"></div>
+  </div>
+</div>
+`;
+
+  const input = root.querySelector("input");
+  const dropdown = root.querySelector(".dropdown");
+  const resultsWrapper = root.querySelector(".results");
+
+  const onInput = async (event) => {
+    const movies = await fetchData(event.target.value);
+    //handling empty responses
+    if (!movies.length) {
+      dropdown.classList.remove("is-active");
+      return;
+    }
+    //clears data after search, instead of appending to previous list
+    resultsWrapper.innerHTML = "";
+    dropdown.classList.add("is-active");
+
+    for (let movie of movies) {
+      const option = document.createElement("a");
+      //some images dont have posters.
+      option.classList.add("dropdown-item");
+      option.innerHTML = renderOption(movie);
+      option.addEventListener("click", () => {
+        dropdown.classList.remove("is-active");
+        input.value = movie.Title;
+        onMovieSelect(movie);
+      });
+      resultsWrapper.appendChild(option);
+    }
+  };
+  input.addEventListener("input", debounce(onInput, 500));
+  //close the dropdown by clicking anywhere on screen
+  //other than dropdown itself
+  document.addEventListener("click", (event) => {
+    if (!root.contains(event.target)) {
+      dropdown.classList.remove("is-active");
+    }
+  });
+};
--- index.js
+++ index.js
@@ -10,56 +10,14 @@ const fetchData = async (searchTerm) => {
   }
   return response.data.Search;
 };
-
-const root = document.querySelector(".autocomplete");
-root.innerHTML = `
-<label for="search"><b>Search for a movie</b></label>
-<input type="text" class="input" />
-<div class="dropdown">
-  <div class="dropdown-menu">
-    <div class="dropdown-content results"></div>
-  </div>
-</div>
-`;
-
-const input = document.querySelector("input");
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
-
-const onInput = async (event) => {
-  const movies = await fetchData(event.target.value);
-  //handling empty responses
-  if (!movies.length) {
-    dropdown.classList.remove("is-active");
-    return;
-  }
-  //clears data after search, instead of appending to previous list
-  resultsWrapper.innerHTML = "";
-  dropdown.classList.add("is-active");
-
-  for (let movie of movies) {
-    const option = document.createElement("a");
-    //some images dont have posters.
+createAutoComplete({
+  root: document.querySelector(".autocomplete"),
+  renderOption: (movie) => {
     const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
-    option.classList.add("dropdown-item");
-    option.innerHTML = `
-    <img src = "${imgSrc}" />${movie.Title}
+    return `
+    <img src = "${imgSrc}" />${movie.Title} (${movie.Year})
     `;
-    option.addEventListener("click", () => {
-      dropdown.classList.remove("is-active");
-      input.value = movie.Title;
-      onMovieSelect(movie);
-    });
-    resultsWrapper.appendChild(option);
-  }
-};
-input.addEventListener("input", debounce(onInput, 500));
-//close the dropdown by clicking anywhere on screen
-//other than dropdown itself
-document.addEventListener("click", (event) => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.remove("is-active");
-  }
+  },
 });
 //show statisticsof a movie when selected from dropdown
 const onMovieSelect = async (movie) => {
